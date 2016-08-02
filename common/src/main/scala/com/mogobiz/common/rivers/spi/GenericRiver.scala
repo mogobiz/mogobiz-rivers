d4794bd9a0cfdbfde5077d76292b3aed665af00a
Stephane Manciot <stephane.manciot@ebiznext.com>
2016-08-02 14:14:19 +0300

    prepare partial publication

--- common/src/main/scala/com/mogobiz/common/rivers/spi/GenericRiver.scala
+++ common/src/main/scala/com/mogobiz/common/rivers/spi/GenericRiver.scala
@@ -55,6 +55,8 @@ import java.util
 import scala.collection.JavaConversions._
 
 class RiverConfig{
+  @BeanProperty var partial: Boolean = false
+  @BeanProperty var idCompany: Long = -1L
   @BeanProperty var idCatalogs: util.List[Long] = List()
   @BeanProperty var debug: Boolean = false
   @BeanProperty var dry_run: Boolean = false
@@ -64,4 +66,6 @@ class RiverConfig{
   @BeanProperty var currencyCode: String = "EUR"
   @BeanProperty var clientConfig: ClientConfig = null
   @BeanProperty var countries: util.List[String] = List("DE","ES","FR","GB","US")
+  @BeanProperty var idCategories: util.List[Long] = List()
+  @BeanProperty var idProducts: util.List[Long] = List()
 }
